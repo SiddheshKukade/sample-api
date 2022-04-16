848b1e26a6d4f34b32c8680d3c813d34309bdcea
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-04-16 15:39:12 +0530

    Update third.js

--- third.js
+++ third.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
   
- const secondFunction=  (req, res, next) => {
+ const thirdFunction=  (req, res, next) => {
         res.json(["Tererererony","qweer","sdfsdfsf","Ginger","Food"]);
 };
 module.exports = secondFunction;
